a4c14aae3d5916c30d4a32a095a915cfb70007f2
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-02-09 11:44:20 -0300

    testando condicional while

---
+++ Java e Orientação a Objetos T4 - ONE/Java jRE e JDK - compile e execute o seu programa/src/lacos/TestaWhile.java
@@ -0,0 +1,16 @@
+package lacos;
+
+public class TestaWhile {
+
+    public static void main(String[] args) {
+
+        int contador = 0;
+
+        while (contador ++ < 10){
+            System.out.println(contador);
+        }
+
+        System.out.println("----------------------");
+        System.out.println(contador);
+    }
+}
