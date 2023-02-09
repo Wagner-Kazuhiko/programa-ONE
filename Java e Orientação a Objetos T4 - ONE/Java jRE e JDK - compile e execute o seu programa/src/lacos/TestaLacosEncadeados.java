6f9076d5c4c5d63cd0bad1494eba419deb6ab77a
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-02-09 12:44:38 -0300

    lacos encadeados

---
+++ Java e Orientação a Objetos T4 - ONE/Java jRE e JDK - compile e execute o seu programa/src/lacos/TestaLacosEncadeados.java
@@ -0,0 +1,12 @@
+package lacos;
+
+public class TestaLacosEncadeados {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i ++){
+            for (int j = 0; j <= 10; j++){
+                System.out.println(i * j);
+            }
+        }
+    }
+}
