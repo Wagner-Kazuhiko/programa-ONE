ddda1bca72fb97223e7ceace8c35d042a3cd6f05
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-02-12 18:33:59 -0300

    bytebank instanciando objetos

--- java oo - entendendo a orientacao a objetos/src/bytebank/CriaConta.java
+++ java oo - entendendo a orientacao a objetos/src/bytebank/CriaConta.java
@@ -12,5 +12,13 @@ public class CriaConta {
         System.out.println("A primeira conta tem " + primeiraConta.saldo);
         System.out.println("A segunda conta tem " + segundaConta.saldo);
 
+        System.out.println(primeiraConta.numero);
+        System.out.println(primeiraConta.agencia);
+
+        System.out.println(segundaConta.agencia);
+
+        segundaConta.agencia = 146;
+        System.out.println("Agora a segunda conta esta na " +  "agência " + segundaConta.agencia);
+
     }
 }
