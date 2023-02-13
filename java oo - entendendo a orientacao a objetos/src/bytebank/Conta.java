9ce47f5daf285824c1e1da1e4ef5107a895a83a1
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-02-13 20:05:39 -0300

    metodos java

--- java oo - entendendo a orientacao a objetos/src/bytebank/Conta.java
+++ java oo - entendendo a orientacao a objetos/src/bytebank/Conta.java
@@ -10,4 +10,27 @@ public class Conta {
     public void deposita(double valor) {
         this.saldo += valor;
     }
+
+    public boolean saca(double valor){
+        if (this.saldo >= valor){
+            this.saldo -= valor;
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    public  boolean transfere(double valor, Conta destino){
+        if (saldo >= valor){
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
 }
--- java oo - entendendo a orientacao a objetos/src/bytebank/TestaMetodo.java
+++ java oo - entendendo a orientacao a objetos/src/bytebank/TestaMetodo.java
@@ -9,5 +9,24 @@ public class TestaMetodo {
         contaDoPaulo.deposita(50);
 
         System.out.println(contaDoPaulo.saldo);
+
+        contaDoPaulo.saca(20);
+
+        System.out.println(contaDoPaulo.saldo);
+
+        Conta contaMarcela = new Conta();
+        contaMarcela.deposita(1000);
+        System.out.println(contaMarcela.saldo);
+
+        boolean sucessoTransferencia = contaMarcela.transfere(300, contaDoPaulo);
+
+        if (sucessoTransferencia){
+            System.out.println("transferencia com sucesso");
+        }
+        else{
+            System.out.println("faltou dinheiro");
+        }
+        System.out.println(contaMarcela.saldo);
+        System.out.println(contaDoPaulo.saldo);
     }
 }
