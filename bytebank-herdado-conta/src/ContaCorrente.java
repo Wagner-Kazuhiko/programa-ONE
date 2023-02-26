d6b3fce198c2eecbed4e8ae5aeee0dc3a79a8469
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-02-26 16:18:54 -0300

    classes e metodos abstratos

--- bytebank-herdado-conta/src/ContaCorrente.java
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -4,9 +4,15 @@ public class ContaCorrente extends Conta{
         super(agencia, numero);
     }
 
+
     @Override
     public boolean saca(double valor) {
         double valorSacar = valor + 0.2;
         return super.saca(valorSacar);
+    }
+
+    @Override
+    public void deposita(double valor) {
+        super.saldo += valor;
     }
 }
