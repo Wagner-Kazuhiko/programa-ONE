41148766666b259c9d53a656464725724b47ece2
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-02-27 20:19:14 -0300

    interface

--- bytebank-herdado/src/TesteGerente.java
+++ bytebank-herdado/src/TesteGerente.java
@@ -1,6 +1,8 @@
 public class TesteGerente {
     public static void main(String[] args) {
 
+        Autenticavel referencia = new Cliente();
+
         Gerente g1 = new Gerente();
         g1.setNome("Marco");
         g1.setCpf("111222333-44");
@@ -10,12 +12,12 @@ public class TesteGerente {
         System.out.println(g1.getCpf());
         System.out.println(g1.getSalario());
 
-        /*
+
         g1.setSenha(2222);
         boolean auntenticou = g1.autentica(2222);
 
         System.out.println(auntenticou);
-         */
+
         System.out.println(g1.getBonificacao());
     }
 }
