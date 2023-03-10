5515624fba6aaac1dcedea6020336e71065f646c
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-03-09 22:05:01 -0300

    exceções

--- Java e Orientação a Objetos T4 - ONE/bytebank-herdado-conta/src/TesteContas.java
+++ Java e Orientação a Objetos T4 - ONE/bytebank-herdado-conta/src/TesteContas.java
@@ -1,6 +1,9 @@
 public class TesteContas {
     public static void main(String[] args) {
 
+        int a = 3;
+        int b = a / 0;
+
         ContaCorrente outra = null;
         outra.deposita(200.0);
 
