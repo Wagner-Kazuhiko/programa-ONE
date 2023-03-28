bc02dd049844a030c051934221cff755e0eac7b8
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-03-27 21:17:20 -0300

    relacionamentos

--- Java e Orientação a Objetos T4 - ONE/gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ Java e Orientação a Objetos T4 - ONE/gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -1,6 +1,5 @@
 package br.com.alura;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -29,5 +28,12 @@ public class Curso {
     public void adiciona(Aula aula){
         this.aulas.add((aula));
     }
+    public int getTempoTotal(){
+        return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+    }
 
+    @Override
+    public String toString() {
+        return "[Curso: " + this.nome + ", tempo total: " + this.getTempoTotal() + ", " + " aulas: " + this.aulas + "]";
+    }
 }
--- Java e Orientação a Objetos T4 - ONE/gerenciador-de-cursos/src/br/com/alura/TestaCurso2.java
+++ Java e Orientação a Objetos T4 - ONE/gerenciador-de-cursos/src/br/com/alura/TestaCurso2.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class TestaCurso {
+public class TestaCurso2 {
     public static void main(String[] args) {
 
         Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira" );
 
         javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
         javaColecoes.adiciona(new Aula("Criando uma aula", 20));
-        javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
+        javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
 
         //pode ser feito assim
         //javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList", 21));
@@ -25,6 +25,9 @@ public class TestaCurso {
         Collections.sort(aulas);
 
         System.out.println(aulas);
+        System.out.println(javaColecoes.getTempoTotal());
+
+        System.out.println(javaColecoes);
 
 
     }
