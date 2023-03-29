2258b035de2da293a750ee5cef0f8bd07eca1748
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-03-29 12:48:41 -0300

    Collection, Set, HashSet, equals

--- Java e Orientação a Objetos T4 - ONE/gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
+++ Java e Orientação a Objetos T4 - ONE/gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -10,7 +10,7 @@ public class TestaCursoComAluno {
         javaColecoes.adiciona(new Aula("Criando uma aula", 20));
         javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
 
-        Aluno a1 = new Aluno("Rodeigo Turini", 34672);
+        Aluno a1 = new Aluno("Rodrigo Turini", 34672);
         Aluno a2 = new Aluno("Guilherme Silveira", 5617);
         Aluno a3 = new Aluno("Rodeigo Turini", 17645);
 
@@ -22,6 +22,20 @@ public class TestaCursoComAluno {
         javaColecoes.getAlunos().forEach(a -> {
             System.out.println(a);
         });
+
+        System.out.println("O aluno está matriculado?");
+        System.out.println(javaColecoes.estaMatriculado(a1));
+
+        Aluno turini = new Aluno("Rodrigo Turini", 34672);
+        System.out.println("E esse Turini está matriculado?");
+        System.out.println(javaColecoes.estaMatriculado(turini));
+
+        System.out.println("O a1 é equals ao Turini?");
+        System.out.println(a1.equals(turini));
+
+        // obrigatoriamente o seguinte é true:
+
+        System.out.println(a1.hashCode() == turini.hashCode());
 
     }
 
