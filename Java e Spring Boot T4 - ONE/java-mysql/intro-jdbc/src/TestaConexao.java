149a433530224ff477276867e4e3e297b8f4690b
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-04-08 00:04:22 -0300

    diretorio spring api, jdbc, java web

---
+++ Java e Spring Boot T4 - ONE/java-mysql/intro-jdbc/src/TestaConexao.java
@@ -0,0 +1,9 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestaConexao {
+    public static void main(String[] args) {
+
+        Connection connection = DriverManager.getConnection("jdbc");
+    }
+}
