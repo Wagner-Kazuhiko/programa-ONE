568488511009ffa68793cff1f43bc4f3a13b1b10
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-04-08 19:02:00 -0300

    add path jdbc

--- Java e Spring Boot T4 - ONE/java-mysql/intro-jdbc/src/TestaConexao.java
+++ Java e Spring Boot T4 - ONE/java-mysql/intro-jdbc/src/TestaConexao.java
@@ -1,9 +1,14 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class TestaConexao {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        Connection connection = DriverManager.getConnection("jdbc");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "admin", "Admin@1234");
+
+        System.out.println("Fechando conexão!");
+
+        connection.close();
     }
 }
