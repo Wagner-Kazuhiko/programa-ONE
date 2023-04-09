53c33f2cf8e78cfb125d5a569d47156159627a59
Wagner-Kazuhiko <kazu_hiko@hotmail.com>
2023-04-09 13:27:07 -0300

    conexao com mysql

---
+++ Java e Spring Boot T4 - ONE/java-mysql/intro-jdbc/src/teste/TestaListagem.java
@@ -0,0 +1,26 @@
+package teste;
+import java.sql.*;
+import java.util.List;
+
+public class TestaListagem {
+    public static void main(String[] args) throws SQLException {
+
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "admin", "Admin@1234");
+
+        Statement stm = con.createStatement();
+        boolean resultado = stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+
+        ResultSet rst = stm.getResultSet();
+
+        while (rst.next()){
+            Integer id = rst.getInt("ID");
+            System.out.println(id);
+            String nome = rst.getString("NOME");
+            System.out.println(nome);
+            String descricao = rst.getString("DESCRICAO");
+            System.out.println(descricao);
+        }
+
+        con.close();
+    }
+}
